8fde96c7f53d74759a34edab858894390541d65e
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-06 23:02:30 +0200

    0.1 Create project and add .gitignore

---
+++ src/main/java/com/gardenhouse/gardenhousemanager/GardenHouseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gardenhouse.gardenhousemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenHouseManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardenHouseManagerApplication.class, args);
+    }
+
+}
