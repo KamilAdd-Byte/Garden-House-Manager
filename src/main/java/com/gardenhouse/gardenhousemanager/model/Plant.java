25ead4bad3e31e2ca6e70d3cf48ddfeb46529936
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-20 19:34:22 +0200

    0.2 Create plant model

---
+++ src/main/java/com/gardenhouse/gardenhousemanager/model/Plant.java
@@ -0,0 +1,25 @@
+package com.gardenhouse.gardenhousemanager.model;
+
+public class Plant {
+    private String name;
+
+
+    public Plant(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Plant{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
