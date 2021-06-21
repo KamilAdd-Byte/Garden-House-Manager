2839ae360bd6e913ea9bda6ab4f13a8180405c43
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-21 22:53:14 +0200

    0.13 Added opens package in module and export package in module

--- src/main/java/com/gardenhouse/gardenhousemanager/repository/HerbRepository.java
+++ src/main/java/com/gardenhouse/gardenhousemanager/repository/HerbRepository.java
@@ -2,6 +2,7 @@ package com.gardenhouse.gardenhousemanager.repository;
 
 import com.gardenhouse.gardenhousemanager.model.Herb;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
