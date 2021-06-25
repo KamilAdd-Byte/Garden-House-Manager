fd7454d7eb0968d1c7403cf7690960e7e43a8019
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-25 16:13:21 +0200

    0.14 Added wetness enum for plants

---
+++ src/main/java/com/gardenhouse/gardenhousemanager/control/Wetness.java
@@ -0,0 +1,23 @@
+package com.gardenhouse.gardenhousemanager.control;
+
+public enum Wetness {
+    MINIMUM (40,50),
+    OPTIMUM(40,60),
+    MAXIMUM(70, 90);
+
+    public int minWetness;
+    public int maxWetness;
+
+    Wetness(int minWetness, int maxWetness) {
+        this.minWetness = minWetness;
+        this.maxWetness = maxWetness;
+    }
+
+    @Override
+    public String toString() {
+        return "Wetness{" +
+                "minWetness=" + minWetness +
+                ", maxWetness=" + maxWetness +
+                "} " + super.toString();
+    }
+}
