0fb301a19bd7a3d6d33271919161ae8be45fac34
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-07-07 19:34:45 +0200

    1.7.1 Refactor service weather api

--- src/main/java/com/gardenhouse/gardenhousemanager/serviceImpl/WeatherServiceImpl.java
+++ src/main/java/com/gardenhouse/gardenhousemanager/serviceImpl/WeatherServiceImpl.java
@@ -10,12 +10,19 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class WeatherServiceImpl implements WeatherService {
 
+    private static final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/";
+    private static final String API_KEY = "3229abb2a8b89862edf7adfbeb7ab0df";
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Override
     public WeatherDto getWeather() {
-       String response =  restTemplate.getForObject("http://api.openweathermap.org/data/2.5/weather?q=poznan&APPID=3229abb2a8b89862edf7adfbeb7ab0df&units=metric&lang=pl",String.class);
-       log.info(response);
+        String response = getWeatherForCity("poznan");
+        log.info(response);
         return null;
+    }
+
+    private String getWeatherForCity(String city) {
+        return restTemplate.getForObject(WEATHER_URL + "weather?q={city}&APPID={apiKey}&units=metric&lang=pl",
+                String.class, city, API_KEY);
     }
 }
