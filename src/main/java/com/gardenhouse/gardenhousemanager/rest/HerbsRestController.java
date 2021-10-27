853ec311afd4eba638bf2a4cdb2bbd6b80efc4c0
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-10-28 00:04:26 +0200

    3.6 Get all herbs for rest controller (Postman Test)

--- src/main/java/com/gardenhouse/gardenhousemanager/rest/HerbsRestController.java
+++ src/main/java/com/gardenhouse/gardenhousemanager/rest/HerbsRestController.java
@@ -6,15 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class HerbsRestController {
 
     @Autowired
     HerbsService service;
 
+    @GetMapping("/herbs")
+    public ResponseEntity<List<Herbs>> getAllHerbs () {
+        List<Herbs> herbs = service.allHerbs();
+        return ResponseEntity.ok().body(herbs);
+    }
+
     @PostMapping("/add")
     public ResponseEntity<Herbs> addHerbs(@RequestBody Herbs herbs){
         Herbs save = service.addHerbs(herbs);
         return ResponseEntity.ok().body(save);
     }
+
 }
