c989a92168dd15a6c33aaae51bddbc4cff847765
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-11-23 19:50:53 +0100

    6.0 Implementation interface Menu in WelcomeInApp class #27

--- src/main/java/com/gardenhouse/gardenhousemanager/appconsole/user/basicwelcome/WelcomeInApp.java
+++ src/main/java/com/gardenhouse/gardenhousemanager/appconsole/user/basicwelcome/WelcomeInApp.java
@@ -1,5 +1,6 @@
 package com.gardenhouse.gardenhousemanager.appconsole.user.basicwelcome;
 
+import com.gardenhouse.gardenhousemanager.appconsole.menu.Menu;
 import com.gardenhouse.gardenhousemanager.appconsole.user.User;
 import com.gardenhouse.gardenhousemanager.appconsole.user.UserLogged;
 
@@ -7,20 +8,12 @@ import com.gardenhouse.gardenhousemanager.appconsole.user.UserLogged;
  * This class it's generate title, version and description on user view in console.
  * This class contains special writing MENU.
  */
-public class WelcomeInApp {
-    private static final String title = "\n" +
-            "\n" +
-            " _____   _       __         __              __              __                    ___   ____ ___  ___\n" +
-            "/__  /  (_)___  / /___ _   / /_  ____  ____/ /___ _      __/ /___ _____  ___     |__ \\ / __ \\__ \\<  /\n" +
-            "  / /  / / __ \\/ / __ `/  / __ \\/ __ \\/ __  / __ \\ | /| / / / __ `/ __ \\/ _ \\    __/ // / / /_/ // / \n" +
-            " / /__/ / /_/ / / /_/ /  / / / / /_/ / /_/ / /_/ / |/ |/ / / /_/ / / / /  __/   / __// /_/ / __// /  \n" +
-            "/____/_/\\____/_/\\__,_/  /_/ /_/\\____/\\__,_/\\____/|__/|__/_/\\__,_/_/ /_/\\___/   /____/\\____/____/_/   \n" +
-            "                                                                                                     \n" +
-            "\n";
+public class WelcomeInApp implements Menu {
+
     private static final String version = "Garden Home Management v1.0 \nConsole version\n@Kamil Sulejewski °r 2021\n\n";
     private static final String description = "Miejsce na opis funkcjonalności!!\n\n";
     private static final String titleBasicMenu = "°°°°°°°°°°°°°°°° MENU °°°°°°°°°°°°°°°\n";
-    private static final String basicMenu = "Wybierz: \n0 - Wyjście  \n1 - Lista Ziół  \n2 - Dodaj zioło do panelu\n3 - Moje zioła \n4 - Logowanie\n5 - Panel";
+
     private UserLogged user;
 
     public WelcomeInApp(UserLogged user) {
@@ -37,10 +30,10 @@ public class WelcomeInApp {
     }
 
     public String displayTitleAndVersionApp(){
-        return title + version + description;
+        return title() + version + description;
     }
     public String displayBasicMenu(){
-        return titleBasicMenu + displayAccount() + "\n"+ basicMenu +"\n";
+        return titleBasicMenu + displayAccount() + "\n"+ menuOptions() +"\n";
     }
 
     public String displayAccount(){
@@ -61,5 +54,23 @@ public class WelcomeInApp {
      */
     public static String welcomeLoggedUser(UserLogged user){
         return "Witaj w aplikacji " + user.getName().toUpperCase();
+    }
+
+    @Override
+    public String title() {
+        return "\n" +
+                "\n" +
+                " _____   _       __         __              __              __                    ___   ____ ___  ___\n" +
+                "/__  /  (_)___  / /___ _   / /_  ____  ____/ /___ _      __/ /___ _____  ___     |__ \\ / __ \\__ \\<  /\n" +
+                "  / /  / / __ \\/ / __ `/  / __ \\/ __ \\/ __  / __ \\ | /| / / / __ `/ __ \\/ _ \\    __/ // / / /_/ // / \n" +
+                " / /__/ / /_/ / / /_/ /  / / / / /_/ / /_/ / /_/ / |/ |/ / / /_/ / / / /  __/   / __// /_/ / __// /  \n" +
+                "/____/_/\\____/_/\\__,_/  /_/ /_/\\____/\\__,_/\\____/|__/|__/_/\\__,_/_/ /_/\\___/   /____/\\____/____/_/   \n" +
+                "                                                                                                     \n" +
+                "\n";
+    }
+
+    @Override
+    public String menuOptions() {
+        return "Wybierz: \n0 - Wyjście  \n1 - Lista Ziół  \n2 - Szczegółowe informacje na temat hodowli ziół\n3 - Panel";
     }
 }
